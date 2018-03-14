9f40bb932478a70f29aa38e9cf4bc46176931582
Kim Lyndon <kimmylyndon@gmail.com>
2018-03-14 19:20:59 -0400

    Initial Commit

---
+++ RPS/ViewController.swift
@@ -0,0 +1,25 @@
+//
+//  ViewController.swift
+//  RPS
+//
+//  Created by Kim Lyndon on 3/14/18.
+//  Copyright © 2018 Kim Lyndon. All rights reserved.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view, typically from a nib.
+    }
+
+    override func didReceiveMemoryWarning() {
+        super.didReceiveMemoryWarning()
+        // Dispose of any resources that can be recreated.
+    }
+
+
+}
+
